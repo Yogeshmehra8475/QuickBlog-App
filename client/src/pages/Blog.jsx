d400330337139e27826e6c1b875220b92f5108f2
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-07-27 00:22:15 +0530

    Adding Blog Detail page

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import { useParams } from 'react-router-dom'
+import { blog_data } from '../assets/assets'
+import Navbar from '../components/Navbar'
 
 const Blog = () => {
-  return (
-    <div>Blog</div>
-  )
+  const {id} = useParams()
+
+  const [data, setData] = useState(null)
+
+  const fetchBlogData = async ()=>{
+    const data = blog_data.find(item => item._id === id)
+    setData(data)
+  }
+
+    useEffect(()=>{
+      fetchBlogData()
+    },[])
+
+  return data ? (
+    <div>
+      <Navbar/>
+      
+      <div></div>
+
+      <div></div>
+
+    </div>
+  ) :  <div>Loading...</div>
 }
 
 export default Blog
