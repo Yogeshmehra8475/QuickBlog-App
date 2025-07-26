87d11490e7911962985e1bea228576c4a07e9b34
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-07-26 23:24:05 +0530

    Adding Footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { assets, footer_data } from "../assets/assets";
 
 const Footer = () => {
   return (
@@ -8,7 +9,20 @@ const Footer = () => {
             <img src={assets.logo} alt='logo' className='w-32 sm:w-44'/>
             <p className='max-w-[410px] mt-6'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum unde quaerat eveniet cumque accusamus atque qui error quo enim fugiat?</p>
           </div>
-          <div></div>
+          <div className='flex flex-wrap justify-between w-full md:w-[45%] gap-5' >
+            {footer_data.map((section, index)=>(
+              <div key={index}>
+                <h3 className='font-semibold tetx-base text-gray-900 md:mb-5 mb-2'>{section.title}</h3>
+                <ul className='text-sm space-y-1'>
+                  {section.links.map((link, i)=>(
+                    <li key={i}>
+                      <a href='#' className='hover:underline transition'>{link}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
         </div>
         <p className='py-4 text-center text-sm md:text-base text-gray-500/80'>Copyright 2025 © QuickBlog Yogesh - All Right Reserved.</p>
     </div>
