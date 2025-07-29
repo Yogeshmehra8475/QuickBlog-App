1d3501d6cd6e6d9194a316f6b2483ee1c557c6e8
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-07-29 23:27:24 +0530

    Adding required pages

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const Layout = () => {
-  return (
-    <div>Layout</div>
-  )
-}
+  const nevigate = useNavigate();
 
-export default Layout
+  const logout = ()=>{
+    nevigate("/")
+  }
+
+  return (
+    <>
+      <div>
+        <img
+          src={assets.logo}
+          alt=""
+          className="w-32 sm:w-40 cursor-pointer"
+          onClick={() => nevigate("/")}/>
+          <button onClick={logout} className="text-sm px-8 py-2 bg-primary text-white rounded-full curspr-pointer">Logout</button>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
