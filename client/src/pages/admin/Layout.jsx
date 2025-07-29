c5e470c5faf90d3dc4243ff54db8f2000e98c977
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-07-29 23:41:50 +0530

    Created Dashboard menu

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { assets } from "../../assets/assets";
-import { useNavigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import Sidebar from "../../components/admin/Sidebar";
 
 const Layout = () => {
   const nevigate = useNavigate();
@@ -11,13 +12,17 @@ const Layout = () => {
 
   return (
     <>
-      <div>
+      <div className="flex items-center justify-between py-2 h-[70px] px-4 sm:px-12 border-b border-gray-200" >
         <img
           src={assets.logo}
           alt=""
           className="w-32 sm:w-40 cursor-pointer"
           onClick={() => nevigate("/")}/>
           <button onClick={logout} className="text-sm px-8 py-2 bg-primary text-white rounded-full curspr-pointer">Logout</button>
+      </div>
+      <div className="flex h-[calc(100vh-70px)]">
+        <Sidebar/>
+        <Outlet/>
       </div>
     </>
   );
