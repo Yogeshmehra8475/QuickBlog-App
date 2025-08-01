04c69fde71d01874c57b8c59509931887c6efcba
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-08-01 23:59:21 +0530

    A

--- client/src/pages/admin/ListBlog.jsx
+++ client/src/pages/admin/ListBlog.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ListBlog = () => {
+
+  const [blogs, setBlogs] = useState([]);
+
+  const fetchBlogs = async()=>{
+    setBlogs(blogs_data)
+  }
+  useEffect(()=>{
+    fetchBlogs()
+  },[])
+
   return (
-    <div>ListBlog</div>
+    <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16 bg-blue-50/50'>
+
+    </div>
   )
 }
 
