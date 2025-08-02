7dfa1a1b3dd8ca4e43d0514adf2466608757e87b
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-08-02 15:02:37 +0530

    Adding All list pages

--- client/src/components/admin/Sidebar.jsx
+++ client/src/components/admin/Sidebar.jsx
@@ -15,7 +15,7 @@ const Sidebar = () => {
         <p className='hidden md:inline-block'>Add blogs</p>
       </NavLink>
 
-       <NavLink to= '/admin/lisBlog' className={({isActive})=> `flex items-center gap-3 py-3.5 md:px-9 md:min-w-64 cursor-pointer ${isActive && "bg-primary/10 border-r-4 border-primary"}`}>
+       <NavLink to= '/admin/listBlog' className={({isActive})=> `flex items-center gap-3 py-3.5 md:px-9 md:min-w-64 cursor-pointer ${isActive && "bg-primary/10 border-r-4 border-primary"}`}>
         <img src={assets.list_icon} alt='' className='min-w-4 w-5'/>
         <p className='hidden md:inline-block'>Blog lists</p>
       </NavLink>
