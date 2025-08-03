502426e18c31f2450d817674f96826e8eb25e106
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-08-03 09:23:38 +0530

    Adding add blog page

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../../assets/assets'
 
 const AddBlog = () => {
+
+const [image, setImage] = useState(false);
+const [title, setTitle] = useState('');
+const [subTitle, setSubTitle] = useState('');
+const [category, setCategory] = useState('Startup');
+const [isPublished, setIsPublished] = useState(false);
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>AddBlog</div>
+    <form onSubmit={onSubmitHandler} className='flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll'>
+      <div className='bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded'>
+
+        <p>Upload thumbnail</p>
+        <label htmlFor='image'>
+          <img src={!image ? assets.upload_area : URL.createObjectURL(image)} alt='' className='mt-2 h-16 rounded cursor-pointner'/>
+          <input onChange={()=> setImage(e.target.files[0])} type='file' id='image' hidden required/>
+        </label>
+        <p className='mt-4'>Blog title</p>
+        <input type='text' placeholder='Type here' required className='w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none required' onChange={e => setTitle(e.target.value)} value = {title}/>
+
+        
+      </div>
+
+    </form>
   )
 }
 
