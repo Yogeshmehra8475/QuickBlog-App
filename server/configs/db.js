b3f4d8b7ea38d726e75e133b4cc1e4adae10b969
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-08-05 23:30:24 +0530

    Server Connected

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,8 +3,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try{
-        await mongoose.connect()
+        mongoose.connection.on("connected", ()=> console.log("Database Connected"))
+        await mongoose.connect(`${process.env.MONGODB_URI}/quickblog`)
     } catch(error) {
-
+        console.log(error.message);
     }
 }
+
+export default connectDB;
