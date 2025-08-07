66e72ee42f3658c6a3427ef18631bf8ae26ac86c
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-08-07 23:47:41 +0530

    Created models to save the data in the database

---
+++ server/controllers/blogController.js
@@ -0,0 +1,19 @@
+import Blog from "../models/Blog";
+
+
+export const addBlog = async (req, res)=> {
+    try{
+        const {title, subTitle, description, category, isPublished}= JSON.parse(req.body.blog);
+        const imageFile = req.file;
+
+
+        // Check if all fields are present
+        if(!title ||!description || !category ||!imageFile){
+            return res.json({success: false, message: "Missing required fields" })
+        }
+
+        
+    } catch (error) {
+
+    }
+}
