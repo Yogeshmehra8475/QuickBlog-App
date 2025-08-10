9de69dedfa329cd025049d5433a9d0db9e35e8b6
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-08-10 20:22:35 +0530

    Bhai error aagya h preshaan ho gya hu

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -1,10 +1,11 @@
 import ImageKit from "imagekit";
+import dotenv from "dotenv";
 
-
-var imagekit = new ImageKit({
+dotenv.config();
+const imagekit = new ImageKit({
     publicKey : process.env.IMAGEKIT_PUBLIC_KEY,
     privateKey :  process.env.IMAGEKIT_PRIVATE_KEY,
-    urlEndpoint :  process.env.IMAGEKIT_ENDPOINT_KEY
+    urlEndpoint :  process.env.IMAGEKIT_URL_ENDPOINT
 });
 
 export default imagekit;
