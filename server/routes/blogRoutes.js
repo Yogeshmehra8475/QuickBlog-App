d6a3019364aea8a645e9eb7b742880f621454a96
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-08-12 23:06:00 +0530

    error solved

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { addBlog } from '../controllers/blogController.js'; // Assuming this is your controller file
 import auth from '../middleware/auth.js'; // Your authentication middleware
 import upload from '../middleware/multer.js';
+
 const blogRouter = express.Router();
 
 // Define the route with the multer middleware
