1a200e63f5b09611df92f126854e3f2f2c06262d
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-08-13 05:05:34 +0530

    Added API endpoints

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,5 +1,6 @@
 import imagekit from "../configs/imageKit.js";
 import Blog from "../models/Blog.js";
+import Comment from "../models/Comment.js";
 
 export const addBlog = async (req, res) => {
   try {
@@ -80,14 +81,37 @@ export const deleteBlogById = async (req, res) => {
   }
 };
 
-export const togglePublish = async (req, res) =>{
-    try{
-        const { id } = req.body;
-        const blog = await Blog.findById(id);
-        blog.isPublished = !blog.isPublished;
-        await blog.save();
-        res.json({ success: true, message: "Blog status updated" });
-    } catch (error) {
-        res.json({success: false, message: error.message})
-    }
-}
+export const togglePublish = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const blog = await Blog.findById(id);
+    blog.isPublished = !blog.isPublished;
+    await blog.save();
+    res.json({ success: true, message: "Blog status updated" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const addComment = async (req, res) => {
+  try {
+    const { blog, name, content } = req.body;
+    await Comment.create({ blog, name, content });
+    res.json({ success: true, message: "Comment added for review" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getBlogComments = async (req, res) => {
+  try {
+    const { blogId } = req.body;
+    const comments = await Comment.find({
+      blog: blogId,
+      isApproved: true,
+    }).sort({ createdAt: -1 });
+    res.json({ success: true, comments });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
