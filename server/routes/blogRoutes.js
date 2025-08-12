39a77547dda41ce75b9ef7401bc08a8424a9ca8f
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-08-13 04:36:57 +0530

    Adding several Api's which will perform certain actions

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addBlog } from '../controllers/blogController.js'; // Assuming this is your controller file
+import { addBlog, deleteBlogById, getAllBlogs, getBlogById, togglePublish } from '../controllers/blogController.js'; // Assuming this is your controller file
 import auth from '../middleware/auth.js'; // Your authentication middleware
 import upload from '../middleware/multer.js';
 
@@ -7,5 +7,9 @@ const blogRouter = express.Router();
 
 // Define the route with the multer middleware
 blogRouter.post('/add', auth, upload.single('image'), addBlog);
+blogRouter.get("/all", getAllBlogs);
+blogRouter.get("/:blogId", getBlogById);
+blogRouter.post("/delete", auth, deleteBlogById);
+blogRouter.post("/toggle-publish", auth, togglePublish);
 
 export default blogRouter;
