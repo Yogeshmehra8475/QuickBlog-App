b6aa62d0f3e0d4d266ce76cb81b1d3bf73197c03
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-08-14 22:55:52 +0530

    Connecting Frontend with Backend

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useContext, useState } from "react";
+import axios from "axios";
+import {useNavigate} from 'react-router-dom';
+
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+
+const AppContext = createContext();
+
+export const AppProvider =({children}) => {
+
+    const navigate = useNavigate()
+
+    const [token, setToken] = useState(null)
+    const [blog, setBlogs] = useState([])
+    const [input, setInput] = useState("")
+
+    const value = {
+        axios, navigate, token, setToken, blog, setBlogs, input, setInput
+    }
+
+    return (
+
+        <AppContext.Provider value={value}>
+            { children }
+        </AppContext.Provider>
+    )
+}
+
+export const useAppContext = () => {
+    return useContext(AppContext)
+};
