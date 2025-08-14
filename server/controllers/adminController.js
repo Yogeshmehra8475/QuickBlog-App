47d183766980502dd710e4f2cd6bcdf236b3b389
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-08-14 20:40:26 +0530

    Added API endpoints

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken"
 import Blog from "../models/Blog.js";
 import Comment from "../models/Comment.js";
+import { Component } from "react";
 
 export const adminLogin = async (req, res) => {
     try {
@@ -29,6 +30,21 @@ export const getAllComments = async (req, res )=> {
     try {
      const comments = await Comment.find({}).populate("blog").sort({createdAt: -1})
      res.json({success: true, comments})
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
+export const getDashboard = async (req, res) => {
+    try {
+        const recentBlogs = await Blog.find({}).sort({createdAt: -1}).limit(5);
+        const blogs = await Blog.countDocuments();
+        const comments = await Component.countDocuments()
+        const drafts = await Blog.countDocuments({isPublished: false})
+
+        const dashboardData = {
+            blogs, comments, drafts, recentBlogs
+        }
+        res.json({success: true, dashboardData})
     } catch (error) {
         res.json({success: false, message: error.message})
     }
