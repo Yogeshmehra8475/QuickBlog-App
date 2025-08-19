85c1548b314f3f3426795bbc4601230e8604c86a
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-08-19 23:25:45 +0530

    Completed adding all the funcctionalities

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { assets } from "../../assets/assets";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Sidebar from "../../components/admin/Sidebar";
+import { useAppContext } from "../../context/AppContext";
 
 const Layout = () => {
-  const nevigate = useNavigate();
+  const {axios, setToken, navigate} = useAppContext
 
   const logout = ()=>{
-    nevigate("/")
+    localStorage.removeItem('token');
+    axios.defaults.headers.common['Authorization'] = null;
+    setToken(null)
+    navigate("/")
   }
 
   return (
@@ -17,7 +21,7 @@ const Layout = () => {
           src={assets.logo}
           alt=""
           className="w-32 sm:w-40 cursor-pointer"
-          onClick={() => nevigate("/")}/>
+          onClick={() => navigate("/")}/>
           <button onClick={logout} className="text-sm px-8 py-2 bg-primary text-white rounded-full curspr-pointer">Logout</button>
       </div>
       <div className="flex h-[calc(100vh-70px)]">
