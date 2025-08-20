f0f3e3561dce9c2fe78426c739875cc12433c4b3
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-08-20 12:15:49 +0530

    ..

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Header from '../components/Header'
-import BlogList from '../components/BlogList'
-import Newsletter from '../components/NewsLetter'
-import Footer from '../components/Footer'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Header from "../components/Header";
+import BlogList from "../components/BlogList";
+import Footer from "../components/Footer";
+import Newsletter from "../components/Newsletter";
 
 const Home = () => {
   return (
     <>
-     <Navbar/>
-     <Header/>
-     <BlogList/>
-     <Newsletter/>
-     <Footer/>
+      <Navbar />
+      <Header />
+      <BlogList />
+      <Newsletter />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
